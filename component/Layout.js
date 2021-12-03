e3662cae2129048fbfc8df37f71952b246d4d71d
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-12-03 16:23:20 +0000

    template literal in meta tag image

--- component/Layout.js
+++ component/Layout.js
@@ -5,21 +5,19 @@ import { faBehance } from '@fortawesome/free-brands-svg-icons';
 import { faDribbble } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import Navbar from './Navbar';
-// import photo from '../public/photo.svg';
+import { useRouter } from 'next/dist/client/router';
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
-        {/* <title></title> */}
-        {/* <meta
-          name='description'
-          content='Digital and Graphic Designer based in London, UK.'
-        /> */}
+        <meta property='og:title' content='Lisa Borrelli' />
         <meta property='og:url' content='www.lisa-borrelli.com' />
         <meta property='og:type' content='website' />
-        <meta property='og:image' content={'https://lisa-portfolio-8s63b0tfc-alessioitalo.vercel.app/photo.svg'} />
-        <meta property='og:title' content='Lisa Borrelli' />
+        {/* <meta property="og:image" content="http://f97b-2a02-c7f-d493-1300-7c0c-4f4f-efaf-9be4.ngrok.io/preview.jpg" /> */}
+        <meta property='og:image' content={`${router.pathname}/preview.jpg`} />
         <meta
           property='og:description'
           content='Digital and Graphic Designer based in London, UK.'
