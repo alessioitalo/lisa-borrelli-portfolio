d40aebec2b6e0156a98a929712dd5075fd34d8e8
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-12-09 22:44:47 +0000

    created unique classes for each images container

--- pages/[project].js
+++ pages/[project].js
@@ -36,6 +36,10 @@ export const getStaticProps = async ({ params }) => {
   };
 };
 
+{
+  /* <Image src={`https:${image.fields.file.url}`} layout='fill' /> */
+}
+
 const project = ({ project }) => {
   return (
     <Layout>
@@ -46,28 +50,26 @@ const project = ({ project }) => {
             backgroundImage: `url(https:${project.fields.mainImage.fields.file.url})`,
           }}
         >
-          {/* <div className={styles.mainOverlay} /> */}
-          {/* <Image
-            src={`https:${project.fields.mainImage.fields.file.url}`}
-            layout='fill'
-          /> */}
+          <div className={styles.mainOverlay} />
         </div>
         <div className={styles.description}>
           <h1>{project.fields.description.content[0].content[0].value}</h1>
           <h3>{project.fields.description.content[1].content[0].value}</h3>
           <p>{project.fields.description.content[2].content[0].value}</p>
         </div>
-        {project.fields.images.map((image) => {
-          return (
-            <img
-              src={`https:${image.fields.file.url}`}
-              key={image.fields.file.fileName}
-              style={{width: '100%', overflow: 'hidden'}}
-            />
-          );
-        })}
+        <div className={`${project.fields.url} ${styles.imagesContainer}`}>
+          {project.fields.images.map((image) => {
+            return (
+              <img
+                key={image.fields.file.fileName}
+                src={`https:${image.fields.file.url}`}
+                loading='lazy'
+                alt={image.fields.file.fileName}
+              />
+            );
+          })}
+        </div>
       </div>
-      {/* <Image src={`https:${image.fields.file.url}`} width='1000' height='800' /> */}
     </Layout>
   );
 };
