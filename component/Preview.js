0d16c879487e1b131b94ca718b7325e1cdd07a40
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-12-14 11:10:27 +0000

    showing previews of other projects, and code referenced

--- component/Preview.js
+++ component/Preview.js
@@ -2,14 +2,12 @@ import styles from './Preview.module.css';
 import Link from 'next/link';
 
 const Preview = ({ project }) => {
-  console.log(project);
   return (
     <Link href={`/${project.fields.url}`}>
       <a>
         <div className={styles.preview}>
           <h5>{project.fields.name}</h5>
           <img src={project.fields.mainImage.fields.file.url} />
-          {/* <img src={`https:${project.fields.file.url}`} /> */}
         </div>
       </a>
     </Link>
--- pages/[project].js
+++ pages/[project].js
@@ -1,40 +1,34 @@
 import { BiArrowToTop } from 'react-icons/bi';
 import styles from './Project.module.css';
 import Layout from '../component/Layout';
-// import Preview from '../component/Preview';
+import Preview from '../component/Preview';
 import { createClient } from 'contentful';
 import ReactPlayer from 'react-player';
 import { useState, useEffect } from 'react';
 
+// connecting to Contentful, getting Props and Paths
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 });
-
 export const getStaticPaths = async () => {
   const response = await client.getEntries({
     content_type: 'project',
   });
-
   const paths = response.items.map((item) => {
     return { params: { project: item.fields.url } };
   });
-
   return {
     paths,
     fallback: false,
   };
 };
-
 export const getStaticProps = async ({ params }) => {
   const response = await client.getEntries({
     content_type: 'project',
-    // 'fields.url': params.project,
   });
-
   return {
     props: {
-      // project: response.items[0],
       projects: response.items,
       project: response.items.find(
         (item) => item.fields.url === params.project
@@ -43,9 +37,11 @@ export const getStaticProps = async ({ params }) => {
   };
 };
 
+// actual component - takes in TWO PROPS: array of projects and selected project
 const project = ({ projects, project }) => {
-  const [showTopButton, setShowTopButton] = useState(false);
 
+  // return to Top button must only be visible after scroll. Initializing state, then Effect fired with eventListener
+  const [showTopButton, setShowTopButton] = useState(false);
   const topButtonHandler = () => {
     if (window.scrollY >= 400) {
       setShowTopButton(true);
@@ -53,7 +49,6 @@ const project = ({ projects, project }) => {
       setShowTopButton(false);
     }
   };
-
   useEffect(() => {
     document.addEventListener('scroll', topButtonHandler);
     return () => {
@@ -61,21 +56,11 @@ const project = ({ projects, project }) => {
     };
   }, [showTopButton]);
 
-  // getting slugs for previous and next project. If looking at first project, previous will be the last - if looking at the last, next will be the [0]
-  // const currentIndex = projects.indexOf(
-  //   projects.find((item) => item.fields.url === project.fields.url)
-  // );
-
-  // const previousProject =
-  //   projects[currentIndex - 1] || projects[projects.length - 1];
-
-  // const nextProject = projects[currentIndex + 1] || projects[0];
-
-  // projects.splice(projects.indexOf(project), 1);
-
+  // actual component being returned
   return (
     <Layout>
       <div className={styles.container}>
+      {/* // main image of project */}
         <div
           className={styles.mainImage}
           style={{
@@ -84,20 +69,23 @@ const project = ({ projects, project }) => {
         >
           <div className={styles.mainOverlay} />
         </div>
+        {/* // description of project */}
         <div className={styles.description}>
           <h1>{project.fields.description.content[0].content[0].value}</h1>
           <h3>{project.fields.description.content[1].content[0].value}</h3>
           <p>{project.fields.description.content[2].content[0].value}</p>
         </div>
-
+          {/* // return to Top button */}
         <div
           className={`${styles.fixedTopButton} ${
             showTopButton ? '' : styles.transparent
           }`}
         >
-          <a href='#top'><BiArrowToTop /></a>
+          <a href='#top'>
+            <BiArrowToTop />
+          </a>
         </div>
-
+          {/* // if video is available, to be rendered here */}
         {project.fields.video && (
           <ReactPlayer
             url={`https:${project.fields.video.fields.file.url}`}
@@ -109,6 +97,7 @@ const project = ({ projects, project }) => {
             style={{ lineHeight: '0' }}
           />
         )}
+        {/* // mapping all images contained in project */}
         <div className={`${project.fields.url} ${styles.imagesContainer}`}>
           {project.fields.images.map((image) => {
             return (
@@ -121,34 +110,20 @@ const project = ({ projects, project }) => {
             );
           })}
         </div>
-        {/* <div className={styles.previewContainer}>
+        {/* // preview container where all the elements of the array projects 
+        // that are not the selected projects are mapped and rendered as preview */}
+        <div className={styles.previewContainer}>
           <h3>You may also be interested in...</h3>
-          <div>
-            <Preview
-              project={projects[Math.floor(Math.random() * projects.length)]}
-            />
-            <Preview
-              project={projects[Math.floor(Math.random() * projects.length)]}
-            />
-            <Preview
-              project={projects[Math.floor(Math.random() * projects.length)]}
-            />
-          </div>
-        </div> */}
-        <div className={styles.buttonsContainer}>
-          {/* <div className={styles.buttons}>
-            <Link href={`/${previousProject.fields.url}`}>
-              <a>Previous Project</a>
-            </Link>
-            <Link href={`/${nextProject.fields.url}`}>
-              <a>Next Project</a>
-            </Link>
-          </div> */}
-          {/* <div className={styles.top}>
-            <a href='#top'>
-              <BiArrowToTop />
-            </a>
-          </div> */}
+          {projects
+            .filter((item) => item.fields.name !== project.fields.name)
+            .map((remainingProject) => {
+              return (
+                <Preview
+                  project={remainingProject}
+                  key={projects.indexOf(remainingProject)}
+                />
+              );
+            })}
         </div>
       </div>
     </Layout>
